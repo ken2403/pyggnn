f811227d53c0771d4efbd7de58d3bf528b197cb5
ken2403 <ken2403.pmtdg@gmail.com>
2022-07-12 12:17:20 +0900

    modify layer name

--- src/pyggnn/nn/out.py
+++ src/pyggnn/nn/out.py
@@ -37,7 +37,7 @@ class Node2Property(nn.Module):
         aggregation = {"add": global_add_pool, "mean": global_mean_pool}
         assert aggr == "add" or aggr == "mean"
         self.aggr = aggr
-        self.layers = nn.Sequential(
+        self.node_transform = nn.Sequential(
             Dense(in_dim, hidden_dim, bias=True),
             Swish(beta),
             Dense(hidden_dim, hidden_dim, bias=True),
@@ -50,6 +50,6 @@ class Node2Property(nn.Module):
         )
 
     def forward(self, x: Tensor, batch: Optional[Tensor] = None) -> Tensor:
-        out = self.layers(x)
+        out = self.node_transform(x)
         out = self.aggregate(out, batch=batch)
         return self.predict(out)
